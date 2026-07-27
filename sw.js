6d90ff2e8eedd998e71406f86464f2f0a388fc04
kevinmaBC <kevin7837669@gmail.com>
2026-07-27 00:26:58 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='pb40-v1';
+const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
+self.addEventListener('fetch',e=>{const req=e.request;if(req.method!=='GET')return;
+  e.respondWith((async()=>{const cached=await caches.match(req);
+    const net=fetch(req).then(res=>{if(res&&res.status===200){const cc=res.clone();caches.open(CACHE).then(c=>c.put(req,cc));}return res;}).catch(()=>cached);
+    return cached||net;})());});
